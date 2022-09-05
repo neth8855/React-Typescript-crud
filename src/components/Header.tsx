8088dd14f3da3d82cce4debe3c80456b55f56316
Kenneth Gunabe <81471127+neth8855@users.noreply.github.com>
2022-09-05 11:07:06 +0800

    Update Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,15 @@
-import * as React from 'react';
-import { Container, Navbar } from 'react-bootstrap';
+import * as React from "react";
+import { Container, Navbar } from "react-bootstrap";
 
-interface IHeaderProps {
-}
+interface IHeaderProps {}
 
 const Header: React.FunctionComponent<IHeaderProps> = (props) => {
-    return (
-        <Navbar fixed="top" bg="dark" variant="dark">
-            <Container>
-                <Navbar.Brand>
-                    React TypeScript Bootstrap Tutorial
-                </Navbar.Brand>
-            </Container>
-      </Navbar>
+  return (
+    <Navbar fixed="top" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand>React TypeScript Bootstrap</Navbar.Brand>
+      </Container>
+    </Navbar>
   );
 };
 
